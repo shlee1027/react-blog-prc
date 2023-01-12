f29e461d9e1771c7f2290f4a64f7b153eff2b561
Sanghyun Lee <shlee1027@daum.net>
2023-01-12 14:40:01 +0900

    props를 응용한 상세페이지 만들기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 /* eslint-disable */
 
-import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 
@@ -11,6 +10,8 @@ function App() {
     "파이썬독학",
   ]);
   let [따봉, 따봉변경] = useState(0);
+  let [modal, setModal] = useState(false);
+  let [title, setTitle] = useState(1);
 
   return (
     <div className="App">
@@ -18,48 +19,72 @@ function App() {
         <h4>ReactBlog</h4>
       </div>
 
-      <button
-        onClick={() => {
-          let copy = [...글제목];
-          copy.sort();
-          글제목변경(copy);
-        }}
-      >
-        가나다순정렬
-      </button>
       <div className="list">
-        <button
-          onClick={() => {
-            let copy = [...글제목];
-            copy[0] = "여자코트 추천";
-            글제목변경(copy);
-          }}
-        >
-          글수정
-        </button>
-
         <h4>
-          {글제목[0]}
+          {글제목[0]}{" "}
           <span
             onClick={() => {
               따봉변경(따봉 + 1);
             }}
           >
-            좋아요👍
-          </span>{" "}
+            👍
+          </span>
           {따봉}
         </h4>
-        <p>2월 17일 발행</p>
       </div>
       <div className="list">
         <h4>{글제목[1]}</h4>
         <p>2월 17일 발행</p>
       </div>
+
       <div className="list">
-        <h4>{글제목[2]}</h4>
+        <h4
+          onClick={() => {
+            setModal(!modal);
+          }}
+        >
+          {글제목[2]}
+        </h4>
         <p>2월 17일 발행</p>
       </div>
+
+      <button
+        onClick={() => {
+          setTitle(0);
+        }}
+      >
+        글제목0
+      </button>
+      <button
+        onClick={() => {
+          setTitle(1);
+        }}
+      >
+        글제목1
+      </button>
+      <button
+        onClick={() => {
+          setTitle(2);
+        }}
+      >
+        글제목2
+      </button>
+
+      {modal == true ? <Modal title={title} 글제목={글제목} /> : null}
     </div>
+  );
+}
+
+function Modal(props) {
+  return (
+    <>
+      <div className="modal">
+        <h4>{props.글제목[props.title]}</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
+        <button>글수정</button>
+      </div>
+    </>
   );
 }
 
